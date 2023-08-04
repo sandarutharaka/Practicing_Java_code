cb07ca9731728f62ed7da1abba312ee429138a3e
sandarutharaka <138965094+sandarutharaka@users.noreply.github.com>
2023-08-04 19:26:25 +0530

    Add files via upload

---
+++ GUIsimplecalc1/src/main/java/com/mycompany/guisimplecalc/GUIsimplecalc.java
@@ -0,0 +1,10 @@
+package com.mycompany.guisimplecalc;
+public class GUIsimplecalc 
+{
+
+    public static void main(String[] args) 
+    {
+        SimpleCalc s=new SimpleCalc();
+        s.show();
+    }
+}
